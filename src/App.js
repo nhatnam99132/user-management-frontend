7f5b8e16c477f36e2f6c4b4dc2e3116438b7faf1
nhatnam99132 <nhatnam99132@gmail.com>
2024-11-09 10:14:50 +0700

    Edit dockerfile

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { getUsers, createUser } from './apiService';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [newUser, setNewUser] = useState({
+    firstName: '',
+    lastName: '',
+    joinedAt: '',
+    isActive: true,
+  });
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -17,11 +23,23 @@ function App() {
     fetchUsers();
   }, []);
 
+  const handleInputChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setNewUser({
+      ...newUser,
+      [name]: type === 'checkbox' ? checked : value,
+    });
+  };
+
   const handleAddUser = async () => {
-    const newUser = { /* user data */ };
+    const userData = {
+      ...newUser,
+      joinedAt: new Date(newUser.joinedAt).toISOString(),
+    };
     try {
-      const createdUser = await createUser(newUser);
+      const createdUser = await createUser(userData);
       setUsers([...users, createdUser]);
+      setNewUser({ firstName: '', lastName: '', joinedAt: '', isActive: true });
     } catch (error) {
       console.error('Error creating user:', error);
     }
@@ -32,9 +50,41 @@ function App() {
       <h1>Users</h1>
       <ul>
         {users.map(user => (
-          <li key={user.id}>{user.name}</li>
+          <li key={user.id}>{user.firstName} {user.lastName}</li>
         ))}
       </ul>
+      
+      <h2>Add New User</h2>
+      <input
+        type="text"
+        name="firstName"
+        value={newUser.firstName}
+        placeholder="First Name"
+        onChange={handleInputChange}
+      />
+      <input
+        type="text"
+        name="lastName"
+        value={newUser.lastName}
+        placeholder="Last Name"
+        onChange={handleInputChange}
+      />
+      <input
+        type="date"
+        name="joinedAt"
+        value={newUser.joinedAt}
+        onChange={handleInputChange}
+      />
+      <label>
+        Active:
+        <input
+          type="checkbox"
+          name="isActive"
+          checked={newUser.isActive}
+          onChange={handleInputChange}
+        />
+      </label>
+      
       <button onClick={handleAddUser}>Add User</button>
     </div>
   );
